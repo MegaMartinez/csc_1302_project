bfabc66738a1c85a041be2fd9df067b61d096597
rhit-martinms <mattm9103@gmail.com>
2023-04-30 16:15:41 -0400

    fixed calc.py

--- src/calc.py
+++ src/calc.py
@@ -1,25 +1,23 @@
 from dataParser import settings
 import pandas as pd # type: ignore
-import numpy as np
-# INPUT: 
-# - The raw data from the website stored in a dataframe.
-# - The settings taken from settings.json
-# OUTPUT:
-# - An array of at least 4 dataframes to be plotted after calculations.
 
-def calc(data: pd.DataFrame) -> list[pd.DataFrame]:   
-    data = data[['submission_date', 'new_case', 'new_death']]
-
-    stat = pd.DataFrame({
-        'mean': [data['new_case'].mean(), data['new_death'].mean()],
-        'median': [data['new_case'].median(), data['new_death'].median()],
-        'variance': [data['new_case'].var(), data['new_death'].var()],
-        'standard deviation': [data['new_case'].std(), data['new_death'].std()]
-    }, index=['new_case', 'new_death'])
-
-    output = pd.DataFrame({'statistics': stat.index})
-    for col in stat.columns:
-        output[col] = stat[col].values
-    output.set_index('statistics', inplace= True)
-    output= output.round(2)
+def calc(data: pd.DataFrame, settings: settings) -> list[list[pd.DataFrame]]:   
+    output: list[list[pd.DataFrame]] = [[], []]
+    stateGroup = data.groupby('state')
+    calcs = stateGroup.aggregate({'new_case':settings.operations, 'new_death':settings.operations})
+    data['submission_date'] = pd.to_datetime(data['submission_date'], format='%m/%d/%Y')
+    data.sort_values(by='submission_date', inplace=True)
+    data.reset_index(drop=True)
+    for state in data['state'].unique():
+        df_dict = {}
+        for op in settings.operations:
+            df_dict[op] = [calcs.loc[state, 'new_case'][op], calcs.loc[state, 'new_death'][op]]
+        df1 = pd.DataFrame(df_dict, index=['new_case', 'new_death'])
+        df1 = df1.round(2)
+        df1.Name = f'{state} calculated'
+        output[1].append(df1)
+        df2: pd.DataFrame = data.loc[data['state'] == state]
+        df2.sort_values(by=['submission_date'])
+        df2.Name = f'{state} over time'
+        output[0].append(df2)
     return output
--- src/plot.py
+++ src/plot.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt # type: ignore
 import pandas as pd # type: ignore
 
 # INPUT: 
-# - An array of dataframes computed in calc.py, be sure to plot them according to settings.
+# - An array of an array of dataframes computed in calc.py, be sure to plot them according to settings.
+# - The first array is an array of dataframes for each state over time. This will be plotted on a line graph.
+# - The second array is an array of calculated dataframes for each state. This will be plotted in a pie chart.
 # - The settings taken from settings.json
 # OUTPUT: None
 
-def plot(data: list[pd.DataFrame], settings: settings) -> None:
+def plot(data: list[list[pd.DataFrame]], settings: settings) -> None:
     # Your code here
     return
